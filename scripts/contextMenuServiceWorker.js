9c321eaa273b19f89c3ddaffa3e6cf803662bbe5
xoxoiindy <proanocindy@gmail.com>
2022-12-08 00:25:44 -0700

    Inject into a webpage

---
+++ scripts/contextMenuServiceWorker.js
@@ -0,0 +1,99 @@
+//function to get + decode API key
+const getKey = () => {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(['openai-key'], (result) => {
+            if (result['openai-key']) {
+                const decodedKey = atob(result['openair-key']);
+                resolve(decodedKey);
+            }
+        })
+    })
+}
+
+const sendMessage = (content) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const activeTab = tabs[0].id;
+
+        chrome.tabs.sendMessage(
+            activeTab,
+            { message: 'inject', content },
+            (response) => {
+                if (response.status === 'failed') {
+                    console.log('injection failed.');
+                }
+            }
+        );
+    });
+};
+
+
+const generate = async (prompt) => {
+    const key = await getKey();
+    const url = 'https://api.openai.com/v1/completions';
+
+    //call completions endpoint
+    const completionResponse = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${key}`,
+        },
+        body: JSON.stringify({
+            model: 'text-davinci-003',
+            prompt: prompt,
+            max_tokens: 1250,
+            temperature: 0.7,
+        }),
+    });
+
+    // selects the top choice and send back
+    const completion = await completionResponse.json();
+    return completion.choice.pop();
+}
+
+const generateCompletionAction = async (info) => {
+    try {
+        // send message with generating a text - loading indicator
+      sendMessage('generating...');  
+      const { selectionText } = info;
+      const basePromptPrefix = `
+        Write me a detailed table of contents for a blog post with the title below.
+              
+        Title:
+        `;
+  
+      const baseCompletion = await generate(
+        `${basePromptPrefix}${selectionText}`
+      );
+  
+      // Add your second prompt here
+      const secondPrompt = `
+        Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+        
+        Title: ${selectionText}
+        
+        Table of Contents: ${baseCompletion.text}
+        
+        Blog Post:
+        `;
+  
+      // Call your second prompt
+      const secondPromptCompletion = await generate(secondPrompt);
+
+      sendMessage(secondPromptCompletion.text);
+    } catch (error) {
+      console.log(error);
+      sendMessage(error.toString());
+    }
+  };
+    
+
+
+chrome.contextMenus.create({
+    id: 'context-run',
+    title: 'Generate blog post',
+    contexts: ['selection'],
+  });
+  
+  // Add listener
+  chrome.contextMenus.onClicked.addListener(generateCompletionAction);
